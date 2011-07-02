ddb92e6ccde402765db541f758a977e9d712c5b8
micah <micah@44a92a27-b0d5-42b7-ae6a-fcca07464902>
2011-07-02 17:59:20 +0000

    moo

--- depends.js
+++ depends.js
@@ -1,24 +1,7 @@
 enyo.depends(
-	"source/solitaire.js",
-	"source/help.js",
-
-	"source/games/demo.js",
-	"source/games/freecell.js",
-	"source/games/klondike.js",
-	"source/games/canfield.js",
-	"source/games/golf.js",
-	"source/games/tripeaks.js",
-	"source/games/spider.js",
-	"source/games/pyramid.js",
-	"source/games/gaps.js",
-	"source/games/acesup.js",
-	"source/games/yukon.js",
-
-	"source/wrand.js",
-	"source/MersenneTwister19937.js",
-	"source/choosegame.js",
-	"source/choosedeal.js",
-	"source/prefs.js",
+	"source/md5.js",
+	"source/hashed-password.js",
 	"source/main.js",
+
 	"css/main.css"
 );
